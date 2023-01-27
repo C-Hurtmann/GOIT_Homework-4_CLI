9dabd00818375f60bf242877610851987ec90504
C-Hurtmann <Constantine2903@gmail.com>
2023-01-27 20:22:46 +0200

    Added all functions

--- Phone book bot/commands.py
+++ Phone book bot/commands.py
@@ -1,5 +1,8 @@
 phone_book = {}
 
+def hello():
+    print('How can I help you?')
+
 
 def add(name: str, phone: str):
     phone_book[phone] = name
@@ -9,15 +12,27 @@ def add(name: str, phone: str):
 def change(old_phone: str, new_phone: str):
     phone_book[new_phone] = phone_book[old_phone]
     del phone_book[old_phone]
+    
+    
+def phone(name):
+    return [k for k, v in phone_book.items() if v == name]
+
+
+def show_all():
+    print(f'Here is your phone book:')
+    for k, v in phone_book.items():
+        print(f'{k:<15}|{v:<10}')
 
 
 def main():
     add('Victor', '+308')
+    add('Victor', '+30899990454')
     add('Bohdan', '+3809995648')
-    print(phone_book)
+    # print(phone_book)
     change('+308', '+38099-299-87-89')
-    print(phone_book)
-    
+    # print(phone_book)
+    print(phone('Victor'))
+    # show_all()
     
 if __name__ == '__main__':
     main()
--- Phone book bot/parcing.py
+++ Phone book bot/parcing.py
@@ -1,15 +1,26 @@
 import re
 
-import commands
-
-main_commands = {'add': commands.add,
-                 'change': commands.change,
-                 'phone': None,
-                 'show all':None
+import commands as cmd
+main_commands = {'add': cmd.add,
+                 'change': cmd.change,
+                 'phone': cmd.phone,
+                 'show all':cmd.show_all
                  }
 
+def find_command_output(func):
+    def inner(x):
+        if isinstance(func(x), str):
+            pass
+        elif func(x):
+            print("I think, I didn't understand you clearly.\nDid you mean", end=' ') 
+            print(*func(x), sep=' or ', end='?\n')
+        else:
+            print("Sorry, I didn't understand you at all") 
+            print('Here in a list of commands i know:', *list(sorted(main_commands.keys())), sep='\n')
+    return inner
 
 
+@find_command_output
 def find_command(user_input: str):
     user_input = user_input.lower()
     if user_input in main_commands: 
@@ -19,18 +30,16 @@ def find_command(user_input: str):
         searching = re.search(r'{}'.format(req), user_input)
         if searching:
             result.append(searching.group())
-    print("I think, I didn't understand you clearly.\nDid you mean", end=' ') #for particylar match
-    print(*result, sep=' or ', end='?\n')  
-        
-    #print("Sorry, I didn't understand you at all") 
-    #print('Here in a list of commands i know:', *list(sorted(requests.keys())), sep='\n')
+    return result #for particylar match
+
         
 
 def testing():
-    # print(find_command('Add someone to mine phone book'))
-    print(find_command('Could YOU ADD this number to my phone book'))
-    # print(find_command('Could you show me phone number without adding to my phone book?'))
-    
+    find_command('add')
+    # find_command('Add someone to mine phone book')
+    # find_command('Could YOU ADD this number to my phone book')
+    # find_command('Could you show me phone number without adding to my phone book?')
+    # find_command('123')
     
 if __name__ == '__main__':
     testing()
